b487798382db88f049d55c2c15d3678c9568f71e
RioTus <137556423+RioTus@users.noreply.github.com>
2024-08-26 11:58:57 -0700

    Update scannerex2.java

--- scannerex2.java
+++ scannerex2.java
@@ -3,6 +3,6 @@ public class Main{
   public static void main (String[] args ){
     Scanner obj = new Scanner(System.in);
     System.out.print("what day is it?");
-    string day = obj.nextLine();
+    String day = obj.nextLine();
   }
 }
