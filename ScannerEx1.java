31ab627704e10150a54e1fd0e9a9fcd44a840501
RioTus <137556423+RioTus@users.noreply.github.com>
2024-08-26 11:56:37 -0700

    Update ScannerEx1.java

--- ScannerEx1.java
+++ ScannerEx1.java
@@ -3,6 +3,6 @@ public class Main{
   public static void main (String[] args ){
     Scanner obj = new Scanner(System.in);
     System.out.print("input an int:");
-    int a = obj.nextint();
+    int a = obj.nextInt();
   }
 }
