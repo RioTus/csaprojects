c94e562464a606ecfb3306d1269711a0bada40e0
RioTus <137556423+RioTus@users.noreply.github.com>
2024-08-26 11:58:00 -0700

    Create scannerex2.java

---
+++ scannerex2.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main{
+  public static void main (String[] args ){
+    Scanner obj = new Scanner(System.in);
+    System.out.print("what day is it?");
+    string day = obj.nextLine();
+  }
+}
