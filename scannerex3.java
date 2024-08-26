f1e1db38eb94aa09caecdfd11cf603f789b501ac
RioTus <137556423+RioTus@users.noreply.github.com>
2024-08-26 14:19:07 -0700

    Create scannerex3.java

---
+++ scannerex3.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+  public static void main (String[] args ){
+    System.out.print("length:");
+    double length = obj.nextDouble();
+    System.out.print("width:");
+    double width = obj.nextDouble();
+    
+  }
+}
