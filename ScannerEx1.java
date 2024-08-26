46ac790302c8ad9d3d3773e9a0e381dfff423918
RioTus <137556423+RioTus@users.noreply.github.com>
2024-08-26 11:46:59 -0700

    Create ScannerEx1.java

---
+++ ScannerEx1.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main{
+  public static void main (String[] args ){
+    Scanner obj = new Scanner(System.in);
+    System.out.print("input an int:");
+    int a = obj.nextint();
+  }
+}
