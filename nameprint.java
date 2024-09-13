50252cb6e8d8a960d7623a800a33458e211e2314
RioTus <137556423+RioTus@users.noreply.github.com>
2024-09-13 11:34:22 -0700

    Create nameprint.java

---
+++ nameprint.java
@@ -0,0 +1,45 @@
+public class Name {
+    public static void main(String[] args) {
+        // First row
+        System.out.print("*****   ");
+        System.out.print("*****   ");
+        System.out.print(" ***    ");
+        System.out.print("*   *   ");
+        System.out.println("*   *   ");
+        
+        // Second row
+        System.out.print("*    *  ");
+        System.out.print("  *     ");
+        System.out.print("*   *   ");
+        System.out.print("**  *   ");
+        System.out.println("**  *   ");
+        
+        // Third row
+        System.out.print("*    *  ");
+        System.out.print("  *     ");
+        System.out.print("*   *   ");
+        System.out.print("* * *   ");
+        System.out.println("* * *   ");
+        
+        // Fourth row
+        System.out.print("*****   ");
+        System.out.print("  *     ");
+        System.out.print("*   *   ");
+        System.out.print("*  **   ");
+        System.out.println("*  **   ");
+        
+        // Fifth row
+        System.out.print("*  *    ");
+        System.out.print("  *     ");
+        System.out.print("*   *   ");
+        System.out.print("*   *   ");
+        System.out.println("*   *   ");
+        
+        // Sixth row
+        System.out.print("*   *   ");
+        System.out.print("*****   ");
+        System.out.print(" ***    ");
+        System.out.print("*   *   ");
+        System.out.println("*   *   ");
+    }
+}
